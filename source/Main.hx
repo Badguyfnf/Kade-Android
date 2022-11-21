658033cecb86a78ae5735bc9db69272eec49554e
Badguyfnf <116070144+Badguyfnf@users.noreply.github.com>
2022-11-21 04:55:00 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -80,8 +80,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 		
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-
+		
 		addChild(game);
 		
 		//#if !mobile
